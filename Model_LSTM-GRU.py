e263e254502cc7d05348543f6a574f7681688a06
YuHan <146173128+YuIlhan1229@users.noreply.github.com>
2025-04-01 08:40:52 +0700

    Update Model_LSTM-GRU.py

--- Model_LSTM-GRU.py
+++ Model_LSTM-GRU.py
@@ -142,7 +142,7 @@ def main():
         # 2) Tải dữ liệu cho tất cả các mã
         all_data = {}
         for ticker in list_ticker:
-            df_ = fetch_stock_data(ticker, start_date, end_date)
+            df_ = fetch_stock_data(ticker, default_start, default_date)
             if df_ is not None and not df_.empty:
                 all_data[ticker] = df_
 
