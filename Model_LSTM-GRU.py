908ec33d1f6b6d5cc194abc29c5f81bae4db5188
YuHan <146173128+YuIlhan1229@users.noreply.github.com>
2025-04-04 14:51:54 +0700

    Update Model_LSTM-GRU.py

--- Model_LSTM-GRU.py
+++ Model_LSTM-GRU.py
@@ -47,10 +47,10 @@ def add_bg_from_local(image_file):
     f"""
     <style>
     .stApp {{
-        background-image: url(data:image/png;base64,{encoded_string.decode()});
-        background-size: cover;
-        background-color: rgba(255, 255, 255, 0.7);
-        background-blend-mode: overlay;
+        background-image: url(data:image/png;base64,{encoded_string.decode()}); # Gán ảnh nền từ base64
+        background-size: cover; # Tự động co giãn ảnh để phủ kín toàn bộ giao diện
+        background-color: rgba(255, 255, 255, 0.85); # Thêm lớp phủ trắng mờ 70% (R=255, G=255, B=255, Alpha=0.7) để nội dung dễ đọc hơn
+        background-blend-mode: overlay; # Pha trộn ảnh nền với lớp phủ trắng ở trên theo kiểu "overlay" (giúp nền mờ nhẹ nhưng vẫn giữ màu gốc)
     }}
     .custom-title {{
         color: #F05454;
